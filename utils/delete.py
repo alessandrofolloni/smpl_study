c7bb909b8b5580fa83f121ba6f095b8a57530966
alessandrofolloni <allefollo@gmail.com>
2024-09-16 11:54:04 +0200

    Modifications

--- utils/delete.py
+++ utils/delete.py
@@ -2,7 +2,7 @@ import os
 import glob
 
 # Specify the directory containing the files to delete
-output_dir = '/Users/alessandrofolloni/PycharmProjects/smpl_study/inspection/results'
+output_dir = '/Users/alessandrofolloni/PycharmProjects/smpl_study/inspection/inspection/results/body_poses_obj'
 
 # Get the list of all files in the directory
 files = glob.glob(os.path.join(output_dir, '*'))
